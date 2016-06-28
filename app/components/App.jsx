5452e18a21fcb680c6d20323862d2f2b95373859
przemwo <przemwo@o2.pl>
2016-06-28 17:18:26 +0200

    delete note - WIP

---
+++ app/components/App.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import uuid from 'uuid';
+import Notes from './Notes';
+
+export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      notes: [
+        {
+          id: uuid.v4(),
+          task: 'Learn react'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Do laundry'
+        }
+      ]
+    };
+  }
+
+  render() {
+    const {notes} = this.state;
+    return (
+      <div>
+        <button onClick={this.addNote}>+</button>
+        <Notes notes={notes} />
+      </div>
+    );
+  }
+
+  addNote = () => {
+    this.setState({
+      notes: [...this.state.notes, {id: uuid.v4(), task: 'New task'}]
+    });
+  }
+
+}
