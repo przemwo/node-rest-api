ae3ec9bf4135d264499a01143f7b1ab1f1eb64eb
przemwo <przemwo@o2.pl>
2017-02-28 07:58:37 +0100

    fb auth wip

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,22 @@
 var User = require('../models/user');
 
 module.exports = function(app, passport) {
+  app.get('/profile', isLoggedIn, function(req, res){
+      process.nextTick(function() {
+        console.log("===============================");
+        console.log('user: ', req.user);
+        console.log('userId:', req.user.facebook.id);
+        console.log("===============================");
+        User.findOne({'facebook.id': req.user.facebook.id}, function(err, user){
+          console.log('dupa');
+          console.log(err);
+          console.log(user);
+        });
+        console.log("==============================");
+        res.send('Profile!');
+      });
+  	});
+
   app.get('/:id?', function(req, res) {
     var newUser = new User();
     console.log(newUser);
@@ -16,3 +32,13 @@ module.exports = function(app, passport) {
       res.redirect('/api/spendings');
   });
 };
+
+
+function isLoggedIn(req, res, next) {
+	if(req.isAuthenticated()){
+    console.log('yes');
+		return next();
+	}
+  console.log('no');
+	res.redirect('/login');
+}
